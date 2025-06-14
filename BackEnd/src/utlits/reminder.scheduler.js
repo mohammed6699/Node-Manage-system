2c7ded6b86151c2c5871aa8e19593062329c4450
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-14 18:08:53 +0300

    feat: integrate email notification sending in reminder scheduling

--- BackEnd/src/utlits/reminder.scheduler.js
+++ BackEnd/src/utlits/reminder.scheduler.js
@@ -1,10 +1,12 @@
 import schedule from 'node-schedule'
+import sendReminderEmail from './email.notifications.js';
 const schedularReminder = (task) => {
     if (task.reminderTime) {
         console.log("Scheduling reminder for task:", task.title);
         const reminderDate2 = new Date(task.reminderTime);
         schedule.scheduleJob(task._id.toString(), reminderDate2, () => {
             console.log(`Reminder for task ${task._id}: ${task.title} at ${task.reminderTime}`)
+            sendReminderEmail(task);
         });
     }
 };
