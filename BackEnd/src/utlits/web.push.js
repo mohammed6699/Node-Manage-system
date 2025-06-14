ab7ba60141d4da80afd64f196cd85ec69d75a92b
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-14 18:05:48 +0300

    feat: implement sendWebPushNotification function for task reminders

--- BackEnd/src/utlits/web.push.js
+++ BackEnd/src/utlits/web.push.js
@@ -10,3 +10,23 @@ webpush.setVapidDetails(
     publicVapidKey,
     privateVapidKey
 );
+
+/**
+ * @param {Object} subscription 
+ * @param {Object} task 
+ */
+const sendWebPushNotification = async (subscription, task) => {
+    const payload = JSON.stringify({
+        title: `Reminder: ${task.title}`,
+        body: `📝 ${task.description}`,
+    });
+
+    try {
+        await webpush.sendNotification(subscription, payload);
+        console.log(" Notification sent for task:", task.title);
+    } catch (error) {
+        console.error(" Failed to send notification:", error);
+    }
+};
+
+export default sendWebPushNotification;
