76b05a46054a81fd02dd0576075feba5dab61fc2
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-14 20:50:54 +0300

    fix: correct casing for task title in reminder logging

--- BackEnd/src/utlits/reminder.scheduler.js
+++ BackEnd/src/utlits/reminder.scheduler.js
@@ -5,10 +5,10 @@ import { getSubscription } from './subscriptionStore.js';
 const schedularReminder = (task) => {
     if (task.reminderTime) {
 
-        console.log("Scheduling reminder for task:", task.title);
+        console.log("Scheduling reminder for task:", task.Title);
         const reminderDate2 = new Date(task.reminderTime);
         schedule.scheduleJob(task._id.toString(), reminderDate2, () => {
-            console.log(`Reminder for task ${task._id}: ${task.title} at ${task.reminderTime}`);
+            console.log(`Reminder for task ${task._id}: ${task.Title} at ${task.reminderTime}`);
             sendReminderEmail(task);
             const subscription = getSubscription()
             console.log(subscription)
